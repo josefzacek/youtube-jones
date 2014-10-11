2b9c72144b809ff68200022fdb3c186a97676c27
josefzacek <joezacek@gmail.com>
2014-10-11 10:52:32 -0700

    youtube davejones/ruby II -  student class

--- students_main.rb
+++ students_main.rb
@@ -14,30 +14,7 @@ herd = []
 # herd[1].add_class('ruby')
 #
 # puts herd[0]
-# puts herd[1]class Student
-
-attr_accessor :name, :sid, :program
-
-def initialize(name, sid, program)
-  @name = name
-  @sid = sid
-  @program = program
-  @classes = []
-end
-
-def add_class(klass)
-  @classes << klass
-end
-
-def remove_class(klass)
-  @classes.delete(klass)
-end
-
-def to_s
-  return "#{@name.capitalize.ljust(15)}#{@sid.ljust(10)}#{@program.capitalize.ljust(15)}#{@classes.join(", ")}"
-end
-
-end
+# puts herd[1]
 
 puts '***'
 
