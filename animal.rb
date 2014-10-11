8ddf8259ec2bb74804edcd9153a55d200474e905
josefzacek <joezacek@gmail.com>
2014-10-11 14:53:42 -0700

    youtube davejones/ruby II -  class inheritance

--- animal.rb
+++ animal.rb
@@ -7,17 +7,38 @@ class Animal
     @color = color
   end
 
+  def speak
+    return "hello my name is #{@name}"
+  end
+  def identify
+    return "my name is #{@name} and I am #{self.class}"
+  end
+
 end
 
 
 class Tiger < Animal
 
   def speak
-    return 'grrrr'
+    #return 'grrrr'
+    #return super  #execute my parents methods
+    return super + " Grrr"  #execute my parents methods and string defined
+
   end
 
 end
 
+
+class Zebra < Animal
+
+  def speak     # needs to be the same method name
+    return super + " Mufassaaa"
+  end
+end
+
+class Hyena < Animal
+
+end
 
 
 tiger = Animal.new("Tiger", "orange")
@@ -30,3 +51,24 @@ puts tiger_one.speak
 puma = Tiger.new('Tina','black')
 puts puma.inspect
 puts puma.speak
+
+
+puts puma.identify
+
+puts tiger.identify
+
+
+cathy = Zebra.new('cathy', 'black and white')
+
+puts cathy.inspect
+
+puts cathy.identify
+
+puts cathy.speak
+
+
+mick = Hyena.new('mick', 'brown')
+
+puts mick.speak
+puts mick.identify
+
