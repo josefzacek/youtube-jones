0752eb2869349cf1434319797c504fdd98dc34fe
josefzacek <joezacek@gmail.com>
2014-10-11 12:20:44 -0700

    youtube davejones/ruby II -  class inheritance

--- animal.rb
+++ animal.rb
@@ -10,8 +10,28 @@ class Animal
 end
 
 
+class Tiger < Animal
+
+  def speak
+    return 'grrrr'
+  end
+
+end
+
+
 
 tiger = Animal.new("Tiger", "orange")
 
+tiger_one = Tiger.new('jim', 'pink')
+
+puma = Tiger.new('Tina','black')
 
 puts tiger.inspect
+
+puts tiger_one.inspect
+
+puts tiger_one.speak
+
+puts puma.inspect
+
+puts puma.speak
