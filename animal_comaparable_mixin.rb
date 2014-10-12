f4c2706f92b7a5e382fdcb39cb56d337c7b3187e
josefzacek <joezacek@gmail.com>
2014-10-11 17:47:01 -0700

    comparable mixin

--- animal_comaparable_mixin.rb
+++ animal_comaparable_mixin.rb
@@ -19,5 +19,42 @@ class Animal
     return "#{@name},#{@color}"
   end
 
+
+  def to_s
+    return "#{@name}, #{@color}"
+  end
+
+  # overwriting ruby compare method
+  def ==(other)
+    self.name == other.name && self.color == other.color
+  end
+
+  # overwriting ruby plus method
+  def +(other)
+    return Animal.new(self.name + "-" + other.name, "#{self.color}-#{other.color}")
+  end
+
 end
+
+a = Animal.new('Cow', 'pink')
+b = Animal.new('Cow', 'pink')
+
+
+if a == b       #calling ==() method declared in Animal class
+  puts 'yes'
+else
+  puts 'no'
+end
+
+#calling to_s method
+puts a.to_s
+
+c = Animal.new('Bull', 'blue')
+d = Animal.new('Cow', 'pink')
+
+
+e = c + d #calling +() method declared in Animal class
+puts e
+
+
 
