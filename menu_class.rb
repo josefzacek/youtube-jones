ecf97a101bca0454a3e28518919663c9d15c003d
josefzacek <joezacek@gmail.com>
2014-10-11 17:47:01 -0700

    comparable mixin

--- menu_class.rb
+++ menu_class.rb
@@ -8,18 +8,43 @@ class Menu
     @menu_args = menu_args
     @quit = @menu_args.length
   end
+  def get_menu_choice
+
+    @menu_args.each_with_index do |item, index|
+      puts "#{index + 1}. #{item}"
+    end
+
+    puts "Enter your choice:"
+    user_choice = gets.to_i
+    return user_choice
+
+
+  end
 
 end
 
 menu = Menu.new(
     'Load character 1',
     'Load characters 2',
+    'Change weapons 2',
     'Fight',
     'Quit'
 )
 
-while (choice = menu.get_menu_choice) != menu.quit
+# puts "enter #{menu.quit} to quit"
+# menu.get_menu_choice
 
+while (choice = menu.get_menu_choice) != menu.quit
+  case choice
+    when 1
+      puts 'Loading character'
+    when 2
+      puts 'Loading character'
+    when 3
+      puts 'Fighting'
+    when 4
+      puts 'Change weapon'
+  end
 end
 
 
