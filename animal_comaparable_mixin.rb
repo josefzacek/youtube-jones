bb108d63ed7582193372d2ed6c72932b5d7a8735
josefzacek <joezacek@gmail.com>
2014-10-11 17:47:01 -0700

    comparable mixin

--- animal_comaparable_mixin.rb
+++ animal_comaparable_mixin.rb
@@ -1,5 +1,7 @@
 class Animal
 
+  include Comparable  #include module Comparable
+
   attr_accessor :name, :color
 
   def initialize(name, color)
@@ -24,9 +26,14 @@ class Animal
     return "#{@name}, #{@color}"
   end
 
-  # overwriting ruby compare method
-  def ==(other)
-    self.name == other.name && self.color == other.color
+  # # overwriting ruby compare method
+  # def ==(other)
+  #   self.name == other.name && self.color == other.color
+  # end
+
+  # use this if Comparable module included
+  def <=>(other)
+    self.name <=> other.name #&& self.color <=> other.color
   end
 
   # overwriting ruby plus method
@@ -40,11 +47,17 @@ a = Animal.new('Cow', 'pink')
 b = Animal.new('Cow', 'pink')
 
 
-if a == b       #calling ==() method declared in Animal class
-  puts 'yes'
-else
-  puts 'no'
-end
+# if a == b       #calling ==() method declared in Animal class
+#   puts 'yes'
+# else
+#   puts 'no'
+# end
+
+
+# when cComparable module included
+puts 'dave' <=> 'karin'   # => -1   less than         d is less than k
+puts 'zdave' <=> 'karin'  # => 1    greater than      z is greater than k
+puts 'karin' <=> 'karin'  # => 0    equal
 
 #calling to_s method
 puts a.to_s
@@ -52,9 +65,20 @@ puts a.to_s
 c = Animal.new('Bull', 'blue')
 d = Animal.new('Cow', 'pink')
 
+puts c < d    # is Bull less than Cow => true
+puts c > d    # is Bull greater than Cow => false
+puts c == d   # is Bull equal Cow => false
+puts c >= d   # is Bull greater than or equal to Cow => false
+puts c != d   # is Bull not equal to Cow => true
+
 
 e = c + d #calling +() method declared in Animal class
 puts e
+
+
+cage = [c,d]
+puts cage.max # returns largest value thanks to module comparable
+puts cage.min # returns lowest value thanks to module comparable
 
 
 
